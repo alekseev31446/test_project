064cb9dc209d8f6046e91c238189b076d521e937
alekseev31446 <alekseev3446@gmail.com>
2023-11-02 12:19:18 +0200

    comments and shortened code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,28 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorld
- */
 @WebServlet("/HelloWorld")
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloWorld() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html");
-        PrintWriter printWriter = response.getWriter();
-        printWriter.println("<h1>Hello World!</h1>");
+		response.getWriter().println("Hello World!");
 	}
 
 }
